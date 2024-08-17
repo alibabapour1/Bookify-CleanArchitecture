f894c3090a839395a42a211083cf54cfc8a7c0d5
alibabapour1 <49041775+alibabapour1@users.noreply.github.com>
2024-08-17 15:55:57 +0330

    Review entity has been created.

---
+++ Bookify.Domain/Reviews/Review.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.SqlTypes;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Reviews.Events;
+using Bookify.Domain.Users;
+
+namespace Bookify.Domain.Reviews;
+
+public sealed class Review:Entity
+{
+    private Review(
+        Guid Id,
+        Guid bookingId,
+        Guid userId,
+        Guid apartmentId,
+        Rating rating,
+        Comment comment,
+        DateTime utcNow
+
+
+        
+        ) 
+        : base(Id)
+    {
+        BookingId = bookingId;
+        UserId = userId;
+        ApartmentId = apartmentId;
+        Rating = rating;
+        Comment = comment;
+        CreatedDateUtc = utcNow;
+
+    }
+
+    public Guid BookingId { get; private set; }
+    public Guid UserId { get; private set; }
+    public Guid ApartmentId { get; private set; }
+    public Rating Rating { get; private set; }
+    public Comment Comment { get; private set; }
+    public DateTime CreatedDateUtc { get; private set; }
+
+
+    public Result<Review> Create(Booking booking,Rating rating,Comment comment,DateTime utcNow)
+    {
+        if (booking.BookingStatus != BookingStatus.Completed)
+        {
+            return Result.Failure<Review>(ReviewErrors.NotEligible);
+        }
+
+        var review = new Review(Guid.NewGuid(), booking.Id, booking.UserId, booking.ApartmentId, rating, comment, utcNow);
+
+        review.RaiseDomainEvents(new ReviewCreatedDomainEvent(review.Id));
+
+        return review;
+
+    }
+
+}
---
+++ Bookify.Domain/Reviews/ReviewErrors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Reviews
+{
+    public static class ReviewErrors
+    {
+        public static Error NotEligible =
+            new("Review.NotEligible", "The Booking Is Not Completed So It Cannot Be Reviewed.");
+    }
+}
