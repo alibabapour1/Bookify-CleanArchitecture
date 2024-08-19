14787c0a3e1bc354721430a5837631088cabdca2
alibabapour1 <49041775+alibabapour1@users.noreply.github.com>
2024-08-19 22:50:25 +0330

    ResereBookingCommand Usecase Implemented followed by cqrs principles.

---
+++ BookigyApplication/Bookings/ReserveBooking/ReserveBookingCommand.cs
@@ -0,0 +1,14 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+
+namespace Bookify.Application.Bookings.ReserveBooking;
+
+public record ReserveBookingCommand(
+    Guid ApartmentId,
+    Guid UserId,
+    DateOnly Start,
+    DateOnly End
+)
+    : ICommand<Guid>;
---
+++ BookigyApplication/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Users;
+
+namespace Bookify.Application.Bookings.ReserveBooking;
+
+public class ReserveBookingCommandHandler :ICommandHandler<ReserveBookingCommand,Guid >
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IApartmentRepository _apartmentRepository;
+    private readonly IBookingRepository _bookingRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IDateTimeProvider _dateTimeProvider;
+    public Task<Result<Guid>> Handle(ReserveBookingCommand request, CancellationToken cancellationToken)
+    {
+        var user = _userRepository.GetByIdAsync(request.UserId);
+        if (user == null)
+        {
+            return Result.Failure<Guid>(Er);
+        }
+
+
+
+        return;
+    }
+}
