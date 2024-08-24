f3f406f092e971eb7c1590b364c11ff02453ae55
alibabapour1 <49041775+alibabapour1@users.noreply.github.com>
2024-08-24 18:41:32 +0330

    Search Apartments Query And QueryHandler Implemented.

---
+++ BookigyApplication/SearchApartments/SearchApartmentsQuery.cs
@@ -0,0 +1,6 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Application.Apartments.SearchApartments;
+
+namespace Bookify.Application.SearchApartments;
+
+public record SearchApartmentsQuery(DateOnly Start , DateOnly End): IQuery<IReadOnlyList<ApartmentResponse>>, IQuery<ApartmentResponse>;
---
+++ BookigyApplication/SearchApartments/SearchApartmentsQueryHandler.cs
@@ -0,0 +1,85 @@
+﻿using Bookify.Application.Abstractions.Data;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Application.Apartments.SearchApartments;
+using Bookify.Application.Bookings.GetBooking;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Bookings;
+using Dapper;
+
+namespace Bookify.Application.SearchApartments;
+
+public class SearchApartmentsQueryHandler:IQueryHandler<SearchApartmentsQuery,IReadOnlyList<ApartmentResponse>>
+{
+    private readonly ISqlConnectionFactory _connectionFactory;
+
+    private static readonly int[] ActiveBookingStatuses =
+    {
+            (int)BookingStatus.Confirmed
+            ,(int)BookingStatus.Completed
+            ,(int)BookingStatus.Reserved
+
+    };
+
+    public SearchApartmentsQueryHandler(ISqlConnectionFactory connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
+
+    public async Task<Result<IReadOnlyList<ApartmentResponse>>> Handle(SearchApartmentsQuery request, CancellationToken cancellationToken)
+    {
+        //Check for OverLapping
+        if (request.Start > request.End)
+        {
+            return new List<ApartmentResponse>();
+        }
+
+        using var connection = _connectionFactory.CreateConnection();
+
+        const string sql = """
+                           SELECT
+                               a.id AS Id,
+                               a.name AS Name,
+                               a.description AS Description,
+                               a.price_amount AS Price,
+                               a.price_currency AS Currency,
+                               a.address_country AS Country,
+                               a.address_state AS State,
+                               a.address_zip_code AS ZipCode,
+                               a.address_city AS City,
+                               a.address_street AS Street
+                           FROM apartments AS a
+                           WHERE NOT EXISTS
+                           (
+                               SELECT 1
+                               FROM bookings AS b
+                               WHERE
+                                   b.apartment_id = a.id AND
+                                   b.duration_start <= @EndDate AND
+                                   b.duration_end >= @StartDate AND
+                                   b.status = ANY(@ActiveBookingStatuses)
+                           )
+                           """;
+
+        var apartment = await connection.QueryAsync<
+            ApartmentResponse, AddressResponse, ApartmentResponse>(sql, 
+            (apartment, address) =>
+            {
+                apartment.Address =address;
+                return apartment;
+            }
+            ,new
+            {
+                request.Start,
+                request.End,
+                ActiveBookingStatuses
+            },
+            //Splits Query On Country Column 
+            splitOn: "Country"
+
+
+        );
+        return apartment.ToList();
+
+    }
+}
