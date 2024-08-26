f545a890122a0b6b3effe0a6aa6a151f1c948d6a
alibabapour1 <49041775+alibabapour1@users.noreply.github.com>
2024-08-26 17:45:06 +0330

    Insfrastructure layer has been created,Also a DependencyInjection has been created for this assembly.

---
+++ Bookify.Infrastructure/Email/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Application.Abstractions.Email;
+
+namespace Bookify.Infrastructure.Email;
+
+public class EmailService: IEmailService
+{
+    public Task SendEmailAsync(Domain.Users.Email recipient, string subject, string body)
+    {
+        return Task.CompletedTask;
+
+    }
+}
