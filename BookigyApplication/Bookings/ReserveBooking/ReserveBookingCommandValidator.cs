f86faac6ae1e844cb47cfd49dd4d7d25188e003a
alibabapour1 <49041775+alibabapour1@users.noreply.github.com>
2024-08-26 16:57:40 +0330

    created a validtion using FluentValidation for the  ReserveBookingCommand.

---
+++ BookigyApplication/Bookings/ReserveBooking/ReserveBookingCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Bookify.Application.Bookings.ReserveBooking;
+
+public class ReserveBookingCommandValidator : AbstractValidator<ReserveBookingCommand>
+{
+    public ReserveBookingCommandValidator()
+    {
+        RuleFor(v => v.UserId).NotEmpty();
+        RuleFor(v => v.ApartmentId).NotEmpty();
+        RuleFor(v => v.Start).LessThan(v => v.End);
+    }
+}
