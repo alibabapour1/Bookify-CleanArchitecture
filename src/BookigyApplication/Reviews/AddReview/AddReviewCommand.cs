5884755ab7a49d66152c53d408cf9cb6653feefc
alibabapour1 <49041775+alibabapour1@users.noreply.github.com>
2024-11-27 13:50:31 +0330

    creating a command handler for Adding a review

---
+++ src/BookigyApplication/Reviews/AddReview/AddReviewCommand.cs
@@ -0,0 +1,7 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Reviews;
+
+namespace Bookify.Application.Reviews.AddReview;
+
+public record AddReviewCommand(Guid BookingId,Rating Rating,Comment Comment) : ICommand<Guid>;
---
+++ src/BookigyApplication/Reviews/AddReview/AddReviewCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Reviews;
+
+namespace Bookify.Application.Reviews.AddReview;
+
+public class AddReviewCommandHandler:ICommandHandler<AddReviewCommand,Guid>
+{
+    private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly IBookingRepository _bookRepository;
+    private readonly IReviewRepository _reviewRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    public AddReviewCommandHandler(IDateTimeProvider dateTimeProvider, IBookingRepository bookRepository, IReviewRepository reviewRepository, IUnitOfWork unitOfWork)
+    {
+        _dateTimeProvider = dateTimeProvider;
+        _bookRepository = bookRepository;
+        _reviewRepository = reviewRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<Guid>> Handle(AddReviewCommand request, CancellationToken cancellationToken)
+    {
+        var booking = await _bookRepository.GetByIdAsync(request.BookingId, cancellationToken);
+        if (booking == null)
+        {
+            return Result.Failure<Guid>(BookingErrors.NotFound);
+        } 
+            
+        var review = Review.Create(booking, request.Rating, request.Comment, _dateTimeProvider.UtcNow);
+
+        _reviewRepository.AddReview(review.Value);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return review.Value.Id;
+    }
+}
