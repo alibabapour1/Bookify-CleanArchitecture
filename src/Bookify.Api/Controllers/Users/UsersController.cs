42a94d81c6e36b6c0fd8cc05c274d3f6b5c985cd
alibabapour1 <49041775+alibabapour1@users.noreply.github.com>
2024-11-27 14:35:32 +0330

    adding GetLoggedInUser endpoint to the User controller

--- src/Bookify.Api/Controllers/Users/UsersController.cs
+++ src/Bookify.Api/Controllers/Users/UsersController.cs
@@ -1,46 +1,70 @@
-﻿using Bookify.Application.Users.LoginUser;
+﻿using Bookify.Application.Users.GetLoggedInUser;
+using Bookify.Application.Users.LoginUser;
 using Bookify.Application.Users.RegisterUser;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Bookify.Api.Controllers.Users
+namespace Bookify.Api.Controllers.Users;
+
+[ApiController]
+[Route("api/users")]
+public class UsersController : ControllerBase
 {
-    [Route("api/users")]
-    [ApiController]
-    public class UsersController : ControllerBase
+    private readonly ISender _sender;
+
+    public UsersController(ISender sender)
     {
-        private readonly ISender _sender;
+        _sender = sender;
+    }
 
-        public UsersController(ISender sender)
+    [HttpGet("me")]
+    public async Task<IActionResult> GetLoggedInUser(CancellationToken cancellationToken)
+    {
+        var query = new GetLoggedInUserQuery();
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return Ok(result.Value);
+    }
+
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(
+        RegisterUserRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new RegisterUserCommand(
+            request.EmailAddress,
+            request.FirstName,
+            request.LastName,
+            request.Password);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        if (result.IsFailure)
         {
-            _sender = sender;
+            return BadRequest(result.Error);
         }
 
-        [AllowAnonymous]
-        [HttpPost("register")]
-        public async Task<IActionResult> RegisterUser(RegisterUserRequest request, CancellationToken cancellationToken)
-        {
-            var user = new RegisterUserCommand(request.FirstName,
-                request.LastName,
-                request.EmailAddress,
-                request.Password);
-            var result = await _sender.Send(user, cancellationToken);
-            return result.IsSuccess ? Ok(result.Value) : BadRequest(result.Error);
+        return Ok(result.Value);
+    }
 
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> LogIn(
+        LoginUserRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new LoginUserCommand(request.Email, request.Password);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return Unauthorized(result.Error);
         }
 
-        [HttpPost("Login")]
-        public async Task<IActionResult> LoginUser(LoginUserRequest request
-            ,CancellationToken cancellationToken)
-        {
-            var command = new LoginUserCommand(request.Email, request.Password);
-            var result = await _sender.Send(command,cancellationToken);
-
-           return result.IsFailure ? Unauthorized(result.Error) : Ok(result.Value) ;
-
-        }
-        
-
+        return Ok(result.Value);
     }
 }
